166fab1ceb0fbe486da1ea2fea3facce98a55e51
haha <afyapu@163.com>
2024-05-06 22:30:33 +0800

    feat:抽离编辑器到package

--- apps/client/components/notion-editor.vue
+++ apps/client/components/notion-editor.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { Editor } from "novel-vue";
+// import { Editor } from "novel-vue";
+import { Editor } from "@brain/editor";
 import "novel-vue/dist/style.css";
 </script>
